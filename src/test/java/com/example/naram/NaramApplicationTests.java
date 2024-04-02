c80932314fe41f3a5c7c89a3843e0d6bb387bd4f
friedSaewoo <zriag@naver.com>
2024-04-02 15:21:29 +0900

    git test

---
+++ src/test/java/com/example/naram/NaramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
